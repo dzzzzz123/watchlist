9427f03ee12f81ee2eb3a00f64ea5b2ff58ae272
Dang Zhang <3109864016@qq.com>
2021-09-20 16:37:12 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from flask import Flask
-from flask_login import LoginManager, login_manager
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith('win')
@@ -12,9 +12,10 @@ else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),
+                                                              os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -26,12 +27,15 @@ def load_user(user_id):
     user = User.query.get(int(user_id))
     return user
 
+
 login_manager.login_view = 'login'
+
 
 @app.context_processor
 def inject_user():
     from watchlist.models import User
     user = User.query.first()
     return dict(user=user)
+
 
 from watchlist import views, errors, commands
